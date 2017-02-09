5d0a632fe08b7d9a124681c3ef91dfd85c5997c8
Sergey Kanzhelev <SergKanz@microsoft.com>
2017-02-08 22:01:56 -0800

    onboarded on Application Insights SDK

--- WebTestsTools/Global.asax.cs
+++ WebTestsTools/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,7 @@ namespace WebTestsTools
     {
         protected void Application_Start()
         {
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new PreserveClientIpTelemetryInitializer());
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
---
+++ WebTestsTools/PreserveClientIpTelemetryInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace WebTestsTools
+{
+    internal class PreserveClientIpTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (!string.IsNullOrEmpty(telemetry.Context.Location.Ip))
+            {
+                telemetry.Context.Properties["ClientIP"] = telemetry.Context.Location.Ip;
+            }
+        }
+    }
+}
